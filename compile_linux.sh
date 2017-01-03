b66de9f90d7d953a219687a73169705247155add
felix <redstorm82@qq.com>
2017-01-03 11:59:01 +0800

    change build script

--- compile_linux.sh
+++ compile_linux.sh
@@ -11,4 +11,3 @@ export NM=nm
 #make V=1
 #make
 make clean && make && make install
-./configure  --without-ca-bundle --enable-static --enable-debug --disable-manual --disable-ipv6 --disable-shared --prefix=$HOME/curl_install/$ARCH
