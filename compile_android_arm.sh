1fe97963d40e2f2401719272a29393ff34d5ee7b
felix <redstorm82@qq.com>
2017-01-03 17:40:55 +0800

    edit build script for android

--- compile_android_arm.sh
+++ compile_android_arm.sh
@@ -1,4 +1,5 @@
 #./buildconf
+#export LD_LIBRARY_PATH=/home/shenshaohui/openssl-install/x86/lib: $LD_LIBRARY_PATH
 export ARCH=arm
 export NDK_ROOT=/opt/ndk/android-ndk-r13b
 export ANDROID_SYSROOT=$NDK_ROOT/platforms/android-18/arch-arm
@@ -14,3 +15,5 @@ export NM=arm-linux-androideabi-nm
 ./configure --host=arm-linux-androideabi --with-ssl=$HOME/openssl-install/$ARCH --disable-manual --disable-ipv6 --disable-shared --disable-FILE --disable-FTP --disable-GOPHER --disable-IMAP --disable-POP3 --disable-RTSP --disable-SMTP --disable-TELNET --disable-TFTP --prefix=$HOME/curl_install/$ARCH
 ##make V=1
 make clean && make && make install
+
+#export LD_LIBRARY_PATH=/data/flygo:$LD_LIBRARY_PATH
